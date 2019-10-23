c06bace467e7e986e069da4fb2fd37d2c1112c80
Жанчипжапов Александр <zhan_chip@mail.ru>
2019-10-23 11:23:00 +0300

    layout

--- Magazin/WebUiAdmin/src/App.vue
+++ Magazin/WebUiAdmin/src/App.vue
@@ -1,19 +1,30 @@
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <div id="nav">
+
+        <el-aside width="200px">
+          <el-menu>
+            <el-menu-item>
+              <router-link to="/">Home</router-link>
+            </el-menu-item>
+            <el-menu-item>
+              <router-link to="/about">About</router-link>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+
+      </div>
+
+      <el-container>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
-
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
