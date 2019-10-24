4bc9e2c8b04d8c9b8e2b2ccc9b0a8ea9066333fc
Жанчипжапов Александр <zhan_chip@mail.ru>
2019-10-24 17:39:56 +0300

    WIP

--- Magazin/WebUiAdmin/src/App.vue
+++ Magazin/WebUiAdmin/src/App.vue
@@ -1,30 +1,11 @@
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
 <template>
   <div id="app">
-    <el-container style="height: 100%; border: 1px solid #eee">
-      <div id="nav">
-
-        <el-aside width="200px">
-          <el-menu>
-            <el-menu-item>
-              <router-link to="/">Home</router-link>
-            </el-menu-item>
-            <el-menu-item>
-              <router-link to="/about">About</router-link>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-
-      </div>
-
-      <el-container>
-        <el-main>
-          <router-view />
-        </el-main>
-      </el-container>
-    </el-container>
-
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
